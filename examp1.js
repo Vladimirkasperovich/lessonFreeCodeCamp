8c688d019012fbb23c69b243da55a764e4cd548a
vladimir kasperovich <92395305+Vladimirkasperovich@users.noreply.github.com>
2023-01-12 16:19:10 +0300

    lesson1
    lesson1

---
+++ examp1.js
@@ -0,0 +1,43 @@
+/*function myLocalScope() {
+    // Only change code below this line
+    let myVar;
+  
+    console.log('inside myLocalScope', myVar);
+  }
+  myLocalScope();
+  
+  // Run and check the console
+  // myVar is not defined outside of myLocalScope
+  console.log('outside myLocalScope', myVar);*/
+
+/************************************************************************* */
+// const outerWear = "T-Shirt";
+
+// function myOutfit() {
+//   // Only change code below this line
+//  let outerWear = 'sweater'
+//   // Only change code above this line
+//   return outerWear;
+// }
+
+// myOutfit();
+
+/**************************************************************************** */
+
+// let sum = 0;
+
+// function addThree() {
+//   sum = sum + 3;
+// }
+
+// // Only change code below this line
+
+// function addFive(){
+//   sum += 5;
+// }
+// // Only change code above this line
+
+// addThree();
+// addFive();
+
+/********************************************************************************* */
